04a581882542b72f7be9e9099cc14e9b4275646c
jakkumame <nkmrysyk0914@gmail.com>
2023-05-15 14:20:15 +0900

    init firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'kamiyado-17f0c',
+    appId: '1:1052305057419:web:f9f39714a6c4526340568e',
+    storageBucket: 'kamiyado-17f0c.appspot.com',
+    apiKey: 'AIzaSyC7hc6KN_aTasWI6mA5sFpng7Zi0DHTvsc',
+    authDomain: 'kamiyado-17f0c.firebaseapp.com',
+    messagingSenderId: '1052305057419',
+    measurementId: 'G-2Y3G1QGS0X',
+  },
   production: true
 };
