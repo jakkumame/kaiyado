3a33d41cb48c0b2af2187c082cabf51f35fa4517
jakkumame <nkmrysyk0914@gmail.com>
2023-05-25 08:47:54 +0900

    form tab g

--- src/app/reservation/usual-edit/usual-edit.page.ts
+++ src/app/reservation/usual-edit/usual-edit.page.ts
@@ -1,7 +1,5 @@
-import { AuthService } from './../../service/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ReserveService } from 'src/app/service/reserve/reserve.service';
 
 
 
@@ -11,35 +9,78 @@ import { ReserveService } from 'src/app/service/reserve/reserve.service';
   styleUrls: ['./usual-edit.page.scss'],
 })
 export class UsualEditPage {
-  form: FormGroup;
+  activeTab = 1;
+
+
+  form1: FormGroup;
+  form2: FormGroup;
+  form3: FormGroup;
   title: string;
   checkInDates: Date[] = [];
   checkOutDates: Date[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
-    private reserveService: ReserveService,
-    private authService: AuthService,
     ) {
-    this.form = this.formBuilder.group({
-      宿泊人数: ['',  ],
-      利用人数: ['',  ],
-      朝食24日: ['',  ],
-      昼食24日: ['',  ],
-      夕食24日: ['',  ],
-      宿泊人数24日: ['',  ],
-      朝食25日: ['',  ],
-      昼食25日: ['',  ],
-      夕食25日: ['',  ],
-      宿泊人数25日: ['',  ],
-      朝食26日: ['',  ],
-      昼食26日: ['',  ],
-      夕食26日: ['',  ],
-      宿泊人数26日: ['',  ],
-      チェックイン日: ['',  ],
-      チェックイン時間: ['',  ],
-      チェックアウト日: ['',  ],
-      チェックアウト時間: ['',  ]
+    this.form1 = this.formBuilder.group({
+      宿泊人数: ['', Validators.required, Validators.min(0) ],
+      利用人数: ['', Validators.required, Validators.min(1) ],
+      朝食24日: ['', Validators.required, Validators.min(0) ],
+      昼食24日: ['', Validators.required, Validators.min(0) ],
+      夕食24日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数24日: ['', Validators.required, Validators.min(0) ],
+      朝食25日: ['', Validators.required, Validators.min(0) ],
+      昼食25日: ['', Validators.required, Validators.min(0) ],
+      夕食25日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数25日: ['', Validators.required, Validators.min(0) ],
+      朝食26日: ['', Validators.required, Validators.min(0) ],
+      昼食26日: ['', Validators.required, Validators.min(0) ],
+      夕食26日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数26日: ['', Validators.required, Validators.min(0) ],
+      チェックイン日: ['', Validators.required ],
+      チェックイン時間: ['', Validators.required ],
+      チェックアウト日: ['', Validators.required ],
+      チェックアウト時間: ['', Validators.required ]
+    });
+    this.form2 = this.formBuilder.group({
+      宿泊人数: ['', Validators.required, Validators.min(0) ],
+      利用人数: ['', Validators.required, Validators.min(1) ],
+      朝食24日: ['', Validators.required, Validators.min(0) ],
+      昼食24日: ['', Validators.required, Validators.min(0) ],
+      夕食24日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数24日: ['', Validators.required, Validators.min(0) ],
+      朝食25日: ['', Validators.required, Validators.min(0) ],
+      昼食25日: ['', Validators.required, Validators.min(0) ],
+      夕食25日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数25日: ['', Validators.required, Validators.min(0) ],
+      朝食26日: ['', Validators.required, Validators.min(0) ],
+      昼食26日: ['', Validators.required, Validators.min(0) ],
+      夕食26日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数26日: ['', Validators.required, Validators.min(0) ],
+      チェックイン日: ['', Validators.required ],
+      チェックイン時間: ['', Validators.required ],
+      チェックアウト日: ['', Validators.required ],
+      チェックアウト時間: ['', Validators.required ]
+    });
+    this.form3 = this.formBuilder.group({
+      宿泊人数: ['', Validators.required, Validators.min(0) ],
+      利用人数: ['', Validators.required, Validators.min(1) ],
+      朝食24日: ['', Validators.required, Validators.min(0) ],
+      昼食24日: ['', Validators.required, Validators.min(0) ],
+      夕食24日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数24日: ['', Validators.required, Validators.min(0) ],
+      朝食25日: ['', Validators.required, Validators.min(0) ],
+      昼食25日: ['', Validators.required, Validators.min(0) ],
+      夕食25日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数25日: ['', Validators.required, Validators.min(0) ],
+      朝食26日: ['', Validators.required, Validators.min(0) ],
+      昼食26日: ['', Validators.required, Validators.min(0) ],
+      夕食26日: ['', Validators.required, Validators.min(0) ],
+      宿泊人数26日: ['', Validators.required, Validators.min(0) ],
+      チェックイン日: ['', Validators.required ],
+      チェックイン時間: ['', Validators.required ],
+      チェックアウト日: ['', Validators.required ],
+      チェックアウト時間: ['', Validators.required ]
     });
     // チェックイン・アウト日の最小値と最大値を設定
     this.generateDates();
