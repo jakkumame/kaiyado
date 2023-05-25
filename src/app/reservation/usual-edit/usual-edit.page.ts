4bdb7d8cee29fa3ed73ff6bd745a0924859c5054
jakkumame <nkmrysyk0914@gmail.com>
2023-05-25 11:07:14 +0900

    firestore save fixed

--- src/app/reservation/usual-edit/usual-edit.page.ts
+++ src/app/reservation/usual-edit/usual-edit.page.ts
@@ -29,70 +29,35 @@ export class UsualEditPage implements OnInit {
     private firestore: AngularFirestore,
     private auth: AngularFireAuth,
     private router: Router,
-    ) {
-    this.form1 = this.formBuilder.group({
+  ) {
+    const formConfig = {
       宿泊人数: ['', Validators.required ],
-      利用人数: ['', Validators.required ],
-      朝食24日: ['', Validators.required ],
-      昼食24日: ['', Validators.required ],
-      夕食24日: ['', Validators.required ],
-      宿泊人数24日: ['', Validators.required ],
-      朝食25日: ['', Validators.required ],
-      昼食25日: ['', Validators.required ],
-      夕食25日: ['', Validators.required ],
-      宿泊人数25日: ['', Validators.required ],
-      朝食26日: ['', Validators.required ],
-      昼食26日: ['', Validators.required ],
-      夕食26日: ['', Validators.required ],
-      宿泊人数26日: ['', Validators.required ],
-      チェックイン日: ['', Validators.required ],
-      チェックイン時間: ['', Validators.required ],
-      チェックアウト日: ['', Validators.required ],
-      チェックアウト時間: ['', Validators.required ],
-    });
-    this.form2 = this.formBuilder.group({
-      宿泊人数: ['', Validators.required, Validators.min(0) ],
-      利用人数: ['', Validators.required, Validators.min(1) ],
-      朝食24日: ['', Validators.required, Validators.min(0) ],
-      昼食24日: ['', Validators.required, Validators.min(0) ],
-      夕食24日: ['', Validators.required, Validators.min(0) ],
-      宿泊人数24日: ['', Validators.required, Validators.min(0) ],
-      朝食25日: ['', Validators.required, Validators.min(0) ],
-      昼食25日: ['', Validators.required, Validators.min(0) ],
-      夕食25日: ['', Validators.required, Validators.min(0) ],
-      宿泊人数25日: ['', Validators.required, Validators.min(0) ],
-      朝食26日: ['', Validators.required, Validators.min(0) ],
-      昼食26日: ['', Validators.required, Validators.min(0) ],
-      夕食26日: ['', Validators.required, Validators.min(0) ],
-      宿泊人数26日: ['', Validators.required, Validators.min(0) ],
-      チェックイン日: ['', Validators.required ],
-      チェックイン時間: ['', Validators.required ],
-      チェックアウト日: ['', Validators.required ],
-      チェックアウト時間: ['', Validators.required ]
-    });
-    this.form3 = this.formBuilder.group({
-      宿泊人数: ['', Validators.required, Validators.min(0) ],
-      利用人数: ['', Validators.required, Validators.min(1) ],
-      朝食24日: ['', Validators.required, Validators.min(0) ],
-      昼食24日: ['', Validators.required, Validators.min(0) ],
-      夕食24日: ['', Validators.required, Validators.min(0) ],
-      宿泊人数24日: ['', Validators.required, Validators.min(0) ],
-      朝食25日: ['', Validators.required, Validators.min(0) ],
-      昼食25日: ['', Validators.required, Validators.min(0) ],
-      夕食25日: ['', Validators.required, Validators.min(0) ],
-      宿泊人数25日: ['', Validators.required, Validators.min(0) ],
-      朝食26日: ['', Validators.required, Validators.min(0) ],
-      昼食26日: ['', Validators.required, Validators.min(0) ],
-      夕食26日: ['', Validators.required, Validators.min(0) ],
-      宿泊人数26日: ['', Validators.required, Validators.min(0) ],
-      チェックイン日: ['', Validators.required ],
-      チェックイン時間: ['', Validators.required ],
-      チェックアウト日: ['', Validators.required ],
-      チェックアウト時間: ['', Validators.required ]
-    });
-    // チェックイン・アウト日の最小値と最大値を設定
+      利用人数: ['', Validators.required],
+      朝食24日: ['', Validators.required],
+      昼食24日: ['', Validators.required],
+      夕食24日: ['', Validators.required],
+      宿泊人数24日: ['', Validators.required],
+      朝食25日: ['', Validators.required],
+      昼食25日: ['', Validators.required],
+      夕食25日: ['', Validators.required],
+      宿泊人数25日: ['', Validators.required],
+      朝食26日: ['', Validators.required],
+      昼食26日: ['', Validators.required],
+      夕食26日: ['', Validators.required],
+      宿泊人数26日: ['', Validators.required],
+      チェックイン日: ['', Validators.required],
+      チェックイン時間: ['', Validators.required],
+      チェックアウト日: ['', Validators.required],
+      チェックアウト時間: ['', Validators.required]
+    };
+
+    this.form1 = this.formBuilder.group(formConfig);
+    this.form2 = this.formBuilder.group(formConfig);
+    this.form3 = this.formBuilder.group(formConfig);
+
     this.generateDates();
   }
+
 
   ngOnInit() {
     // 教会名を取得
@@ -102,8 +67,6 @@ export class UsualEditPage implements OnInit {
       }
     });
   }
-
-
 
 
   generateDates() {
@@ -118,41 +81,34 @@ export class UsualEditPage implements OnInit {
     }
   }
 
+  saveToFirestore(formValue: {}, reservationNumber: string ) {
+    const collectionName = this.churchName;
+
+    this.firestore.collection(collectionName).doc(reservationNumber).set(formValue)
+      .then(() => {
+        this.router.navigateByUrl('/home');
+      })
+      .catch((error) => {
+        console.error('a', error);
+      });
+  }
+
   saveToFirestore1() {
     const formValue = this.form1.value;
-    const collectionName = this.churchName;
-
-    this.firestore.collection(collectionName).doc('reservation1').set(formValue)
-      .then(() => {
-        this.router.navigateByUrl('/home');
-      })
-      .catch((error) => {
-        console.error('a', error);
-      });
+    this.saveToFirestore(formValue, 'reservation1');
   }
+
   saveToFirestore2() {
     const formValue = this.form2.value;
-    const collectionName = this.churchName;
-
-    this.firestore.collection(collectionName).doc('reservation2').set(formValue)
-      .then(() => {
-        this.router.navigateByUrl('/home');
-      })
-      .catch((error) => {
-        console.error('a', error);
-      });
+    this.saveToFirestore(formValue, 'reservation2');
   }
+
   saveToFirestore3() {
     const formValue = this.form3.value;
-    const collectionName = this.churchName;
-
-    this.firestore.collection(collectionName).doc('reservation3').set(formValue)
-      .then(() => {
-        this.router.navigateByUrl('/home');
-      })
-      .catch((error) => {
-        console.error('a', error);
-      });
+    this.saveToFirestore(formValue, 'reservation3');
   }
+
+
+
 
 }
